95880acfd065c4f24cdc4fe86bc76e2fd59f3cfe
ccsosa24 <chrstina.sosa024@gmail.com>
2020-02-11 21:00:16 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Zxcvbnm24!",
-  database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Zxcvbnm24!',
+        database: 'burgers_db'
+    });
+};
+
 
 
 connection.connect(function(err) {
